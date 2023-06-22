80d22ccefb3104f1481a3e1ce119a77ffb30e176
dineshmehra86 <dinesh.mehra86@gmail.com>
2023-06-22 22:45:55 +0530

    concept is updated

---
+++ js/custom.js
@@ -0,0 +1,15 @@
+// JS FAT arrow function 
+// **return statement and curly {} braces is optional when we have only one statement in our function body 
+
+// // 1. Function Defination
+// let myFunctionE = (a, b) => a+b;
+
+// // 2. Function Calling/Invoking
+// console.log(myFunctionE(5,6));
+
+
+// You can also short the function as below option-2
+let myFunctionF = a => a+a;
+
+// Option-2 output
+console.log(myFunctionF(5));
---
+++ js/custom2.js
@@ -0,0 +1,14 @@
+// JS Object Destructuring
+// Destructuring means to extract information
+
+let student = {
+    fname:'Dinesh',
+    lname:'Mehra',
+    mob:'9899495773',
+    address:'New Delhi',
+    age:37
+}
+
+// I want to extract some informtion from the student object is called Destructuring 
+let {mob} = student
+console.log(mob);
